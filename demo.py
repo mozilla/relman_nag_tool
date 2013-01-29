1af801a09f80fd9f3e73d3e8a027c790ecf78f58
Lukas Blakk <lukasblakk@gmail.com>
2013-01-29 15:45:30 -0800

    i'm really messing it up now

--- demo.py
+++ demo.py
@@ -107,17 +107,17 @@ def get_queries():
 
 class Main(flask.views.MethodView):
 	def get(self):
-		return flask.render_template('login.html')
+		return flask.render_template('index.html')
 
 	def post(self):
 		if 'logout' in flask.request.form:
 			flask.session.pop('username', None)
-			return flask.redirect(flask.url_for('login'))
+			return flask.redirect(flask.url_for('index'))
 		required = ['username', 'passwd']
 		for r in required:
 			if r not in flask.request.form:
 				flask.flash("Error: {0} is required.".format(r))
-				return flask.redirect(flask.url_for('login'))
+				return flask.redirect(flask.url_for('index'))
 		username = flask.request.form['username']
 		passwd = flask.request.form['passwd']
 		try:
@@ -127,7 +127,7 @@ class Main(flask.views.MethodView):
 			
 		except Exception:
 			flask.flash("Username doesn't exist or incorrect password")
-			return flask.redirect(flask.url_for('login'))
+			return flask.redirect(flask.url_for('index'))
 		return flask.redirect(flask.url_for('show_templates'))
 		
 def login_required(method):
@@ -139,7 +139,7 @@ def login_required(method):
 				return method(*args, **kwargs)
 			else:
 				flask.flash("a login is required to view this page!")
-				return flask.redirect(flask.url_for('login'))
+				return flask.redirect(flask.url_for('index'))
 		except Exception :
 			print Exception
 	return wrapper
@@ -358,8 +358,10 @@ class Show_Message(flask.views.MethodView):
 		except Exception :
 			print "\nException:"
 			print Exception
+		
 
-app.add_url_rule('/',view_func=Main.as_view('login'), methods=['GET','POST'])
+ 
+app.add_url_rule('/',view_func=Main.as_view('index'), methods=['GET', 'POST'])
 app.add_url_rule('/show_templates', view_func=Show_Templates.as_view('show_templates'), methods=['GET','POST'])
 app.add_url_rule('/create_template', view_func=Create_Template.as_view('create_template'), methods=['GET','POST'])
 app.add_url_rule('/use_template', view_func=Use_Template.as_view('use_template'), methods=['GET','POST'])
